4c1d9e854feaee6d4195bfadce206d3eed727932
Aditya Agrawal <aditya999123@gmail.com>
2022-12-26 10:14:06 +0530

    js logic added

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ function App() {
     setToRefresh(t);
   }
 
+  var proxy_url = window.SUPA_PROXY_URL + '/lua/todo/tasks?limit=5';
+
   return (
     <div>
       <div>
@@ -23,7 +25,7 @@ function App() {
       </div>
       <main className="container">
         <Main title='From Source' source='AWS US-East-1' API_URL='https://demo-origin.trysuperapi.com/todo/tasks?limit=5' toRefresh={toRefresh} />
-        <Main title='From SuperAPI' source='Datacenter closest to user' API_URL='https://blr.trysuperapi.com/lua/todo/tasks?limit=5' toRefresh={toRefresh} />
+        <Main title='From SuperAPI' source='Datacenter closest to user' API_URL={proxy_url} toRefresh={toRefresh} />
       </main>
     </div>
   );
